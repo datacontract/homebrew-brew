9ae66d41d28324cdd752637a39a291f33d7563b1
Stefan Negele <stefan.negele@innoq.com>
2023-10-02 18:36:45 +0200

    Update datacontract.rb

--- Formula/datacontract.rb
+++ Formula/datacontract.rb
@@ -1,7 +1,7 @@
 class Datacontract < Formula
   desc "Manage your datacontract.yaml files"
   homepage "https://cli.datacontract.com/"
-  url "https://github.com/datacontract/cli.git", tag: "v0.2.0", revision: "09da8ceb09a8c4b21303c1af65e65769adbc25f3"
+  url "https://github.com/datacontract/cli.git", tag: "v0.3.0", revision: "bd1fb01d236cd82fd48aae4a993464b259604225"
   license "MIT"
 
   depends_on "go" => :build
@@ -11,6 +11,6 @@ class Datacontract < Formula
   end
 
   test do
-    assert_match "datacontract version v0.2.0", shell_output("#{bin}/datacontract -v")
+    assert_match "datacontract version v0.3.0", shell_output("#{bin}/datacontract -v")
   end
 end
