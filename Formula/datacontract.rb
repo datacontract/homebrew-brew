f8b07f923d91ea1ce4faf6ed672b6c13c384e643
Stefan Negele <stefan.negele@innoq.com>
2023-11-17 15:02:10 +0100

    v0.4.0

--- Formula/datacontract.rb
+++ Formula/datacontract.rb
@@ -1,13 +1,13 @@
 class Datacontract < Formula
   desc "Manage your datacontract.yaml files"
   homepage "https://cli.datacontract.com/"
-  url "https://github.com/datacontract/cli.git", tag: "v0.3.2", revision: "3961a035336a0fa2341d40539f7eda8f7f237a2f"
+  url "https://github.com/datacontract/cli.git", tag: "v0.4.0", revision: "026f328789d8849724d426f41383305d0aa0f717"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "-o", bin/"datacontract"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "-o", bin/"datacontract", "cmd/datacontract.go"
   end
 
   test do
